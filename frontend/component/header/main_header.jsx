4841446d9731256b9b9d3fb1d70cfcd1d99226a9
Anthony <chen.j.anthony@gmail.com>
2021-06-03 18:24:10 -0500

    more styling done, finished sidebar styling and mainnavbar done

--- frontend/component/header/main_header.jsx
+++ frontend/component/header/main_header.jsx
@@ -1,16 +1,69 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 import "./header.css"
+import {FaUserCircle} from 'react-icons/fa'
+import {IconContext} from "react-icons"
+import { connect } from 'react-redux'
+import { logout } from '../../action/session_action'
 
-export default class Mainheader extends React.Component {
+const mSTP = ({session, entities: {users}}) => {
+    return {
+        currentUser: users[session.id],
+    }
+}
+class Mainheader extends React.Component {
     constructor(props) {
         super(props)
+        this.text = {dashboard: "Home", holdings: "Holdings", trade: "Trade"}
+        this.state = {hidden: true}
+        this.toggle = this.toggle.bind(this)
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    toggle(e) {
+        e.preventDefault()
+        if (e.target.className == 'user-dropdown-menu' || e.target.className == 'dropdown-signout') {
+            return
+        }
+        this.setState({ hidden: !this.state.hidden });
+    }
+
+    handleClick(e) {
+        this.props.logout()
     }
 
     render() {
         return (
-            <div className='main_header'>
+            <div className='main-header'>
+                <div className='main-header-left'>{this.text[this.props.location.pathname.slice(1)]}</div>
+                <div className='main-header-right'>
+                    <button className='header-buy-btn'>Buy / Sell</button>
+                    <div className='vertical-line'></div>
+                    <div className='user-menu-dropdown' onClick={this.toggle} tabIndex="1">
+                        <IconContext.Provider value={{style: {color: 'lightgray', fontSize: '34px'}}}>
+                            <FaUserCircle />
+                        </IconContext.Provider>
+                        <span>{this.props.currentUser.firstName}</span>
+                        {!this.state.hidden ?
+                            <div>
+                                <div className="user-dropdown-menu" >
+                                    <IconContext.Provider value={{style: {color: 'lightgray', fontSize: '60px', marginTop: '30px'}}}>
+                                        <FaUserCircle />
+                                    </IconContext.Provider>
+                                    <span style={{marginTop:'20px'}}>{this.props.currentUser.firstName} {this.props.currentUser.lastName}</span>
+                                    <span style={{color:'gray', fontWeight: 400, fontSize: '14px'}}>Username: {this.props.currentUser.username}</span>
+                                    <div className='horizontal-line'></div>
+                                    <div className='dropdown-signout' onClick={this.handleClick}>Sign out</div>
+                                </div>
+                                <div className='graybg' onClick={this.toggle}>
+                                </div>
+                            </div>
+                        : null}
+                    </div>
+                </div>
             </div>
         )
     }
 }
+
+export default withRouter(connect(mSTP, {logout})(Mainheader))
--- frontend/component/sidebar/sidebar.jsx
+++ frontend/component/sidebar/sidebar.jsx
@@ -4,6 +4,10 @@ import { Link, withRouter } from 'react-router-dom'
 import './sidebar.css'
 import { getCurrentPrice } from '../../action/price_action'
 import { updateUi } from '../../action/ui_action'
+import { IoIosHome } from 'react-icons/io'
+import { AiFillPieChart } from 'react-icons/ai'
+import { RiArrowLeftRightFill} from 'react-icons/ri'
+
 
 const mSTP = ({coins}) => {
     return {
@@ -34,10 +38,23 @@ class Sidebar extends React.Component {
             <div className='sidebar'>
                 <button className='sidebar-home-button' onClick={() => this.props.history.replace('/dashboard')}>coinoutpost</button>
                 <ul>
-                    <li className={this.check_path('/dashboard')}><Link to='/dashboard'>Home</Link></li>
-                    <li className={this.check_path('/holdings')}><Link to='/holdings'>Holdings</Link></li>
-                    <li className={this.check_path('/trade')}><Link to='/trade'>Trade</Link></li>
+                    <li className={this.check_path('/dashboard')}>
+                        <div className='sidebar-item' onClick={() => this.props.history.replace('/dashboard')}>
+                            <div className='sidebar-circle'><IoIosHome/></div><span>Home</span>
+                        </div>
+                    </li>
+                    <li className={this.check_path('/holdings')}>
+                        <div className='sidebar-item' onClick={() => this.props.history.replace('/holdings')}>
+                            <div className='sidebar-circle'><AiFillPieChart/></div><span>Holdings</span>
+                        </div>
+                    </li>
+                    <li className={this.check_path('/trade')}>
+                        <div className='sidebar-item' onClick={() => this.props.history.replace('/trade')}>
+                            <div className='sidebar-circle'><RiArrowLeftRightFill/></div><span>Trade</span>
+                        </div>
+                    </li>
                 </ul>
+                <div className='testcircle'></div>        
             </div>
         )
     }
